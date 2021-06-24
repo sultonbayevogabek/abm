608cb682fbc2e5f9c085cdc518ddf1af57713dcd
sultonbayevogabek <sultonbayevogabek@gmail.com>
2021-06-24 17:28:23 +0500

    Tugadi

---
+++ src/public/scripts/data.js
@@ -0,0 +1,368 @@
+const data = [
+   {
+      menu: ['Биз ҳақимизда', 'Сервислар', 'Нега айнан биз?', 'Ҳамкорлар', 'Алоқа'],
+      intro: {
+         h1: 'Биз сизнинг фойдангиз учун таржима қиламиз.',
+         p: 'Ҳозирни ўзида бизга ёзма ёки аудио таржима буюртиринг ва  20% чегирмага эга бўлинг',
+         a: 'Буюртма бериш'
+      },
+      about: {
+         h2: 'Биз ҳақимизда',
+         p: '«ABM INNOVATSIYA INVEST», 2015- йилдан буён таржима хизматлари бозорида фаолият юритиб келмоқда. Биз таклиф этаётган хизматлар қаторига юқори малакали ёзма, оғзаки ва кетма-кет ва синхрон таржима, таржима қилинган ҳужжатларни нотариал тасдиқлаш, уларни қонунийлаштириш / апостилизация (Адлия вазирлиги, Ташқи ишлар вазирлиги, элчихоналарида)  тасдиқлаш киради.'
+      },
+      services: {
+         h2: 'Сервислар',
+         cards: [
+            {
+               h3: 'Шахсий ҳужжатлар',
+               p: 'Шахсий ҳужжатларни (паспорт, диплом, аттестат ва х.к.) бир тилдан бошқа тилга таржима қилиш.',
+               a: 'Буюртма бериш'
+            },
+            {
+               h3: 'В2В таржима'
+            },
+            {
+               h3: 'Сайтларни таржима қилиш'
+            },
+            {
+               h3: 'Тезкор таржима'
+            },
+            {
+               h3: 'В2С таржималар',
+            },
+            {
+               h3: 'Хужжатларни тиклаш',
+            }
+         ]
+      },
+      whyWe: {
+         h2: 'Нега айнан биз?',
+      },
+      trustedCompanies: {
+         h2: 'Бизга ишонч билдирган компаниялар',
+         list: [
+            {
+               logo: 'https://banner2.cleanpng.com/20180502/twq/kisspng-mitsubishi-corporation-company-subsidiary-mitsubis-mitsubishi-motors-5aea28acbb72a3.6889878015252952767678.jpg',
+               webSite: 'https://www.mitsubishicorp.com/'
+            },
+            {
+               logo: 'https://www.developmentaid.org/files/organizationLogos/tokyo-electric-power-services-co-ltd-tepsco-44112.jpg',
+               webSite: 'http://www.tepsco.co.jp/english/index.html'
+            },
+            {
+               logo: 'https://turonbank.uz/bitrix/templates/main/img/header__logo.svg',
+               webSite: 'https://turonbank.uz/'
+            },
+            {
+               logo: 'https://static.tildacdn.com/tild3661-3238-4837-b961-393964323931/photo_2021-02-18_11-.png',
+               webSite: 'https://zizi-uz.all.biz/'
+            }
+         ]
+      },
+      feedback: {
+         h2: 'Биз хакимизда мижозларнинг фикрлари',
+         list: [
+            {
+               author: 'Санжар Норматов',
+               companyName: 'Mitsubishi Corporation',
+               text: '«ABM INNOVATSIYA INVEST», - ҳар доим энг қисқа вақт ичида бажарилган ишларнинг ишончли ва сифатли натижасидир. Мен агентлик билан 5 йилдан кўпроқ вақт давомида ишлаб келаяпман. Тавсия этаман.'
+            },
+            {
+               author: 'Бобур Мухаммедов',
+               companyName: 'TEPSCO',
+               text: 'Босиқ, мулойим ва тезкор ходимлар! Мен бир неча йилдан буён уларнинг хизматларидан фойдаланаман ва ҳар сафар уларнинг мижозларга муносабатидан мамнун бўламан! Бундай иш учун уларга катта раҳмат!'
+            },
+            {
+               author: 'Азизмирза Умедов',
+               companyName: 'JELEGUM GOLD SERVICE',
+               text: 'Бу биринчи навбатда мижозларга йўналтирилган ва мижознинг сўровини қондириш ва уни профессионал даражада бажариш учун ўз фаолиятига устувор аҳамият берадиган Тошкентдаги кам сонли агентликлардан биридир! Мен ходимларнинг ишидан ич ичимдан ҳайратландим ва натижадан мамнунман'
+            }
+         ]
+      },
+      faq: {
+         h2: 'Кўп бериладиган саволлар',
+         list: [
+            {
+               question: 'Қайси тилларда таржима ишларини олиб борасизлар?',
+               answer: 'Ўзбек тили, Рус тили, Инглиз тили, Немис тили, Француз тили, Испан тили, Тожик тили, Хитой тили, Япон тили'
+            }
+         ]
+      },
+      contactUs: {
+         h3: 'Бизнинг менежерларимиз сиз билан боғланишини хоҳласангиз ҳозирни ўзида пастдаги шаклни тўлдиринг',
+         name: 'Исмингиз',
+         phone: 'Телефон рақамингиз',
+         send: 'Жўнатиш'
+      }
+   },
+   {
+      menu: ['Biz haqimizda', 'Servislar', 'Nega aynan biz?', 'Hamkorlar', 'Aloqa'],
+      intro: {
+         h1: 'Biz sizning foydangiz uchun tarjima qilamiz.',
+         p: 'Hozirni o‘zida bizga yozma yoki audio tarjima buyurtiring va  20% chegirmaga ega bo‘ling.',
+         a: 'Buyurtma berish'
+      },
+      about: {
+         h2: 'Biz haqimizda',
+         p: '«ABM INNOVATSIYA INVEST», MChJ 2015- yildan buyon tarjima xizmatlari bozorida faoliyat yuritib kelmoqda. Biz taklif etayotgan xizmatlar qatoriga yuqori malakali yozma, og\'zaki va ketma-ket va sinxron tarjima, tarjima qilingan hujjatlarni notarial tasdiqlash, ularni qonuniylashtirish / apostilizatsiya (Adliya vazirligi, Tashqi ishlar vazirligi, elchixonalarida) tasdiqlash kiradi.'
+      },
+      services: {
+         h2: 'Servislar',
+         cards: [
+            {
+               h3: 'Shaxsiy hujjatlar',
+               p: 'Shaxsiy hujjatlarni (pasport, diplom, attestat va h.k.) bir tildan boshqa tilga tarjima qilish',
+               a: 'Buyurtma berish'
+            },
+            {
+               h3: 'В2В tarjima'
+            },
+            {
+               h3: 'Saytlarni tarjima qilish'
+            },
+            {
+               h3: 'Tezkor tarjima'
+            },
+            {
+               h3: 'В2С tarjimalar'
+            },
+            {
+               h3: 'Hujjatlarni tiklash',
+            }
+         ]
+      },
+      whyWe: {
+         h2: 'Nega aynan biz?',
+      },
+      trustedCompanies: {
+         h2: 'Bizga ishonch bildirgan kompaniyalar',
+         list: [
+            {
+               logo: 'https://banner2.cleanpng.com/20180502/twq/kisspng-mitsubishi-corporation-company-subsidiary-mitsubis-mitsubishi-motors-5aea28acbb72a3.6889878015252952767678.jpg',
+               webSite: 'https://www.mitsubishicorp.com/'
+            },
+            {
+               logo: 'https://www.developmentaid.org/files/organizationLogos/tokyo-electric-power-services-co-ltd-tepsco-44112.jpg',
+               webSite: 'http://www.tepsco.co.jp/english/index.html'
+            },
+            {
+               logo: 'https://turonbank.uz/bitrix/templates/main/img/header__logo.svg',
+               webSite: 'https://turonbank.uz/'
+            },
+            {
+               logo: 'https://static.tildacdn.com/tild3661-3238-4837-b961-393964323931/photo_2021-02-18_11-.png',
+               webSite: 'https://zizi-uz.all.biz/'
+            }
+         ]
+      },
+      feedback: {
+         h2: 'Biz haqimizda mijozlarning fikrlari',
+         list: [
+            {
+               author: 'Sanjar Normatov',
+               companyName: 'Mitsubishi Corporation',
+               text: '«ABM INNOVATSIYA INVEST», - har doim eng qisqa vaqt ichida bajarilgan ishlarning ishonchli va sifatli natijasidir. Men agentlik bilan 5 yildan ko\'proq vaqt davomida ishlab kelayapman. Tavsiya etaman.'
+            },
+            {
+               author: 'Bobur Muxammedov',
+               companyName: 'TEPSCO',
+               text: '«ABM INNOVATSIYA INVEST», - har doim eng qisqa vaqt ichida bajarilgan ishlarning ishonchli va sifatli natijasidir. Men agentlik bilan 5 yildan ko\'proq vaqt davomida ishlab kelayapman. Tavsiya etaman.'
+            },
+            {
+               author: 'Azizmirza Umedov',
+               companyName: 'JELEGUM GOLD SERVICE',
+               text: 'Bu birinchi navbatda mijozlarga yo\'naltirilgan va mijozning so\'rovini qondirish va uni professional darajada bajarish uchun o\'z faoliyatiga ustuvor ahamiyat beradigan Toshkentdagi kam sonli agentliklardan biridir! Men xodimlarning ishidan ich ichimdan hayratlandim  va natijadan mamnunman.'
+            }
+         ]
+      },
+      faq: {
+         h2: 'Ko‘p beriladigan savollar',
+         list: [
+            {
+               question: 'Qaysi tillarda tarjima ishlarini olib borasizlar?',
+               answer: 'O`zbek tili, Rus tili, Ingliz tili, Nemis tili, Fransuz tili, Ispan tili, Tojik tili, Xitoy tili, Yapon tili'
+            }
+         ]
+      },
+      contactUs: {
+         h3: 'Bizning menejerlarimiz siz bilan bog‘lanishini xohlasangiz hozirni o‘zida pastdagi shaklni to‘ldiring.',
+         name: 'Ismingiz',
+         phone: 'Telefon raqamingiz',
+         send: 'Jo`natish'
+      }
+   },
+   {
+      menu: ['О нас', 'Сервисы', 'Почему именно мы?', 'Партнеры', 'Контакты'],
+      intro: {
+         h1: 'Мы переводим для Вашей пользы.',
+         p: 'Закажите нам письменный или аудио перевод прямо сейчас и получите скидку 20 %.',
+         a: 'Заказ'
+      },
+      about: {
+         h2: 'О нас',
+         p: '«ABM INNOVATION INVEST» работает на рынке переводческих услуг с 2015 года. Услуги, которые мы предлагаем, включают в себя высококвалифицированный письменный, устный, последовательный и синхронный перевод, нотариальное заверение переведенных документов, их легализацию / апостилирование (в Министерстве юстиции, Министерстве иностранных дел, посольствах).'
+      },
+      services: {
+         h2: 'Сервисы',
+         cards: [
+            {
+               h3: 'Личные документы',
+               p: 'Перевод личных документов (паспорт, диплом, аттестат и т.д.) с одного языка на другой.',
+               a: 'Заказ'
+            },
+            {
+               h3: 'Перевод В2В'
+            },
+            {
+               h3: 'Перевод сайтов'
+            },
+            {
+               h3: 'Быстрый перевод'
+            },
+            {
+               h3: 'Переводы В2С'
+            },
+            {
+               h3: 'Восстановление документов'
+            }
+         ]
+      },
+      whyWe: {
+         h2: 'Почему именно мы?',
+      },
+      trustedCompanies: {
+         h2: 'Компании, которые нам доверяют',
+         list: [
+            {
+               logo: 'https://banner2.cleanpng.com/20180502/twq/kisspng-mitsubishi-corporation-company-subsidiary-mitsubis-mitsubishi-motors-5aea28acbb72a3.6889878015252952767678.jpg',
+               webSite: 'https://www.mitsubishicorp.com/'
+            },
+            {
+               logo: 'https://www.developmentaid.org/files/organizationLogos/tokyo-electric-power-services-co-ltd-tepsco-44112.jpg',
+               webSite: 'http://www.tepsco.co.jp/english/index.html'
+            },
+            {
+               logo: 'https://turonbank.uz/bitrix/templates/main/img/header__logo.svg',
+               webSite: 'https://turonbank.uz/'
+            },
+            {
+               logo: 'https://static.tildacdn.com/tild3661-3238-4837-b961-393964323931/photo_2021-02-18_11-.png',
+               webSite: 'https://zizi-uz.all.biz/'
+            }
+         ]
+      },
+      feedback: {
+         h2: 'Отзывы клиентов о нас',
+         list: [
+            {
+               author: 'Санжар Норматов',
+               companyName: 'Mitsubishi Corporation',
+               text: '«ABM INNOVATSIYA INVEST» - всегда надежный и качественный результат работы, выполненной в кратчайшие сроки. Я работаю с агентством более 5 лет. Рекомендую.'
+            },
+            {
+               author: 'Бобур Мухаммедов',
+               companyName: 'TEPSCO',
+               text: 'Сдержанный, вежливый и оперативный персонал! Я пользуюсь их услугами уже несколько лет и каждый раз получаю удовольствие от их отношения к клиентам! Большое им спасибо за такую работу!'
+            },
+            {
+               author: 'Азизмирза Умедов',
+               companyName: 'JELEGUM GOLD SERVICE',
+               text: 'Это одно из немногих агентств в Ташкенте, которое в первую очередь ориентировано на клиента и уделяет приоритетное внимание своей деятельности, чтобы удовлетворить запрос клиента и выполнить его на профессиональном уровне! Я внутренне впечатлен работой сотрудников и доволен результатом.'
+            }
+         ]
+      },
+      faq: {
+         h2: 'Часто задаваемые вопросы',
+         list: [
+            {
+               question: 'На каких языках Вы выполняете работу по переводу.',
+               answer: 'Узбекский язык, Русский язык, Английский язык, Немецкий язык, Французский язык, Испанский язык, Таджикский язык, Китайский язык, Японский язык'
+            }
+         ]
+      },
+      contactUs: {
+         h3: 'Если Вы хотите, чтобы наши менеджеры связались с Вами, заполните форму ниже прямо сейчас.',
+         name: 'Ваше имя',
+         phone: 'Ваш номер телефона',
+         send: 'Отправка'
+      }
+   },
+   {
+      menu: ['About us', 'Services', 'Why us?', 'Partners', 'Contacts'],
+      intro: {
+         h1: 'We translate for your benefit.',
+         p: 'Order a written or audio translation now and get a 20% discount.',
+         a: 'Order'
+      },
+      about: {
+         h2: 'About us',
+         p: '"ABM INNOVATION INVEST" has been working on the translation services market since 2015. The services we offer include highly qualified translation, interpretation, consecutive and simultaneous interpretation, notarial certification of translated documents, legalization / apostille (at the Ministry of Justice, the Ministry of Foreign Affairs, embassies).'
+      },
+      services: {
+         h2: 'Services',
+         cards: [
+            {
+               h3: 'Personal documents',
+               p: 'Translation of personal documents (passport, diploma, certificate of secondary education, etc.) from one language to another',
+               a: 'Order'
+            },
+            {
+               h3: 'B2B translation'
+            },
+            {
+               h3: 'Web-sites translation'
+            },
+            {
+               h3: 'Rapid translation'
+            },
+            {
+               h3: 'B2C translations'
+            },
+            {
+               h3: 'Re-issue of documents'
+            }
+         ]
+      },
+      whyWe: {
+         h2: 'Why us?',
+      },
+      trustedCompanies: {
+         h2: 'Companies that trust us'
+      },
+      feedback: {
+         h2: 'Customer feedback',
+         list: [
+            {
+               author: 'Sanjar Normatov ',
+               companyName: 'Mitsubishi Corporation',
+               text: '“ABM INNOVATSIYA INVEST” it’s always a reliable and quality result of work done in the shortest possible time. I have been working with the agency for over 5 years. My recommendation'
+            },
+            {
+               author: 'Bobur Mukhammedov',
+               companyName: 'TEPSCO',
+               text: 'Discreet, attentive and responsive staff! I have been resorting to their services for several years and every time I enjoy their attitude towards their customers! Many thanks for their work!'
+            },
+            {
+               author: 'Azizmirza Umedov',
+               companyName: 'JELEGUM GOLD SERVICE',
+               text: 'This is one of the few agencies in Tashkent, which is primarily client-oriented and prioritizes its activities to meet the client’s request and fulfill it professionally! I am truly impressed with the work of the staff and pleased with the result.'
+            }
+         ]
+      },
+      faq: {
+         h2: 'FAQs',
+         list: [
+            {
+               question: 'What languages do you translate?',
+               answer: 'Uzbek language, Russian language, English language, German language, French language, Spanish language, Tajik language, Chinese language, Japanese language'
+            }
+         ]
+      },
+      contactUs: {
+         h3: 'If you want our managers to contact you, fill out the form below right now. ',
+         name: 'Your name',
+         phone: 'Your phone',
+         send: 'Send'
+      }
+   }
+]
---
+++ src/public/scripts/languages.js
@@ -0,0 +1,241 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+   const selectOne = selector => document.querySelector(selector),
+      selectAll = selector => document.querySelectorAll(selector)
+
+   function addClass(element, className) {
+      element.classList.add(className)
+   }
+
+   function removeClass(element, className) {
+      element.classList.remove(className)
+   }
+
+   new Splide( '.splide', {
+      type: 'loop',
+      perPage: 4,
+      perMove: 1,
+      pagination: false,
+      pauseOnHover: true,
+      autoplay: true,
+      interval: 2000,
+      breakpoints: {
+         800: {
+            perPage: 3,
+         },
+         540: {
+            perPage: 2,
+         },
+      }
+   }).mount();
+
+   function feedbackListRender() {
+      setTimeout(() => {
+         new Splide( '.feedback .splide', {
+            rewind: true,
+            type: 'fade',
+            perPage: 1,
+            perMove: 1,
+            pagination: false,
+            pauseOnHover: true,
+            autoplay: true,
+            interval: 2000,
+         }).mount();
+      }, 200)
+   }
+
+   feedbackListRender()
+
+   const selectElement = selectOne('.select'),
+      selectSelected = selectOne('.select__selected'),
+      selectList = selectOne('.select__list'),
+      selectItems = selectAll('.select__item'),
+      rowLanguageItems = selectAll('.row-language__item')
+
+   const siteNavLinks = selectAll('.site-nav__link--only'),
+      introHeading = selectOne('.site-intro__left h1'),
+      introText = selectOne('.site-intro__left p'),
+      introLink = selectOne('.site-intro__left a'),
+      aboutUsHeading = selectOne('.about-us h2'),
+      aboutUsText = selectOne('.about-us p'),
+      servicesHeading = selectOne('.services h2'),
+      servicesCardsWrapper = selectOne('.services-cards'),
+      whyWeHeading = selectOne('.why-we h2'),
+      whyWeCardsWrapper = selectOne('.why-we__cards'),
+      trustedCompaniesHeading = selectOne('.trusted-companies h2'),
+      trustedCompaniesList = selectOne('.splide__list'),
+      feedbackHeading = selectOne('.feedback h2'),
+      feedbackList = selectOne('.feedback ul'),
+      faqHeading = selectOne('.faq h2'),
+      faqList = selectOne('.faq__ul'),
+      contactUsHeading = selectOne('.contact-us h3'),
+      contactUsNameInput = selectOne('input[name="name"]'),
+      contactUsPhoneInput = selectOne('input[name="phone"]'),
+      contactUsSubmit = selectOne('.contact-us__submit')
+
+   function showSelectList() {
+      removeClass(selectList, 'd-none')
+   }
+
+   function hideSelectList() {
+      addClass(selectList, 'd-none')
+   }
+
+   selectSelected.addEventListener('click', e => {
+      if (selectList.classList.contains('d-none')) {
+         showSelectList()
+      } else {
+         hideSelectList()
+      }
+   })
+
+   let selectedOptionIndex
+
+   if (localStorage.getItem('selectedOptionIndex')) {
+      selectedOptionIndex = localStorage.getItem('selectedOptionIndex')
+      selectItems.forEach(item => item.classList.remove('select__item--active'))
+      addClass(selectItems[selectedOptionIndex], 'select__item--active')
+   } else {
+      selectedOptionIndex = 0
+      selectItems.forEach(item => item.classList.remove('select__item--active'))
+      addClass(selectItems[selectedOptionIndex], 'select__item--active')
+   }
+
+   selectSelected.textContent = selectItems[selectedOptionIndex].textContent
+
+   languageRender(selectedOptionIndex)
+
+   selectList.addEventListener('click', e => {
+      const target = e.target
+      selectItems.forEach((item, index) => {
+         removeClass(item, 'select__item--active')
+
+         if (target === item) {
+            selectedOptionIndex = index
+            selectSelected.textContent = selectItems[selectedOptionIndex].textContent
+            localStorage.setItem('selectedOptionIndex', selectedOptionIndex)
+         }
+      })
+
+      addClass(target, 'select__item--active')
+      languageRender(selectedOptionIndex)
+      feedbackListRender()
+   })
+
+   window.addEventListener('keydown', e => {
+      if (e.code === 'Escape') {
+         hideSelectList()
+      }
+   })
+
+   window.addEventListener('click', e => {
+      if (e.target !== selectSelected) {
+         hideSelectList()
+      }
+   })
+
+   rowLanguageItems[selectedOptionIndex].classList.add('row-language__item--active')
+
+   rowLanguageItems.forEach((item, index) => {
+      item.addEventListener('click', e => {
+         selectedOptionIndex = index
+         localStorage.setItem('selectedOptionIndex', selectedOptionIndex)
+         rowLanguageItems.forEach(item => item.classList.remove('row-language__item--active'))
+         rowLanguageItems[selectedOptionIndex].classList.add('row-language__item--active')
+      })
+   })
+
+   function languageRender(selectedLanguageIndex) {
+      const {menu, intro, about, services, whyWe, trustedCompanies, feedback, faq, contactUs} = data[selectedLanguageIndex]
+
+      for (let i = 0; i < 5; i++) {
+         siteNavLinks[i].textContent = menu[i]
+      }
+
+      introHeading.textContent = intro.h1
+      introText.textContent = intro.p
+      introLink.textContent = intro.a
+
+      aboutUsHeading.textContent = about.h2
+      aboutUsText.textContent = about.p
+
+      servicesHeading.textContent = services.h2
+      servicesCardsWrapper.innerHTML = `
+         <div class="services-cards__left" data-aos="zoom-in" data-aos-delay="100" data-aos-once="true">
+             <div class="services-card">
+                <img src="img/services/1.svg" alt="">
+                <h3>${services.cards[0].h3}</h3>
+                <p>${services.cards[0].p}</p>
+                <a href="#contact">${services.cards[0].a}</a>
+             </div>
+          </div>
+         <div class="services-cards__right" data-aos="zoom-in" data-aos-delay="100" data-aos-once="true">
+             <div class="services-card">
+                <img src="img/services/2.svg" alt="">
+                <h3>${services.cards[1].h3}</h3>
+             </div>
+             <div class="services-card">
+                <img src="img/services/3.svg" alt="">
+                <h3>${services.cards[2].h3}</h3>
+             </div>
+             <div class="services-card">
+                <img src="img/services/4.svg" alt="">
+                <h3>${services.cards[3].h3}</h3>
+             </div>
+             <div class="services-card">
+                <img src="img/services/5.svg" alt="">
+                <h3>${services.cards[4].h3}</h3>
+             </div>
+             <div class="services-card">
+                <img src="img/services/6.svg" alt="">
+                <h3>${services.cards[5].h3}</h3>
+             </div>
+          </div>
+      `
+
+      whyWeHeading.textContent = whyWe.h2
+
+      trustedCompaniesHeading.textContent = trustedCompanies.h2
+
+      feedbackHeading.textContent = feedback.h2
+      feedbackList.innerHTML = ''
+      feedback.list.forEach(item => {
+         feedbackList.innerHTML += `
+            <li class="splide__slide">
+                <h3>
+                   <strong>${item.author}</strong>
+                   <span>${item.companyName}</span>
+                </h3>
+                <p>${item.text}</p>
+            </li>
+         `
+      })
+
+      faqHeading.textContent = faq.h2
+      faqList.innerHTML = ''
+      faq.list.forEach(item => {
+         faqList.innerHTML += `
+            <li class="faq__li" data-aos="fade-right" data-aos-delay="100" data-aos-once="true">
+             <div class="faq__li__header">
+                <h3 class="faq__question">${item.question}</h3>
+                <div class="faq__icon">
+                   <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M7.66878 0.849854V8.08944M7.66878 15.329V8.08944M7.66878 8.08944H14.9084H0.429199"
+                         stroke="" stroke-width="2" />
+                   </svg>
+                </div>
+             </div>
+             <p class="faq__answer">
+                ${item.answer}
+             </p>
+          </li>
+         `
+      })
+
+      contactUsHeading.textContent = contactUs.h3
+      contactUsNameInput.setAttribute('placeholder', contactUs.name)
+      contactUsPhoneInput.setAttribute('placeholder', contactUs.phone)
+      contactUsSubmit.textContent = contactUs.send
+   }
+})
